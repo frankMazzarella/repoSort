861d9f9d20d9f3988695e39f195ad0102679faf9
Frank Mazzarella <mazzarella.frank@gmail.com>
2016-08-16 10:39:07 -0400

    Update app.js
    added more semicolons to please the syntax gods

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 /*
     Sort the Arch Linux repos
-
     This script will create output.json in the directory
     that it is sitting in. It grabs the JSON from
     https://www.archlinux.org/mirrors/status/json/
     and will spit out only US results sorted by score.
     If you need to reverse the direction of the sort,
     please see the documentation in sortThatShit()
-
     NOTE:
     i didnt test this much at all cause family guy is on
 */
@@ -24,19 +22,19 @@ https.get(url, function(res) {
 
     res.on('data', function(chunk) {
         body += chunk;
-    })
+    });
 
     res.on('end', function() {
         var obj = JSON.parse(body);
         obj = sortThatShit(obj);
         writeToFile(obj);
-    })
+    });
 
     // I have no idea if this is actually works
     res.on('error', function(err) {
-        console.log('Shit just happened: ' + err)
-    })
-})
+        console.log('Shit just happened: ' + err);
+    });
+});
 
 function sortThatShit(obj) {
 
@@ -56,7 +54,7 @@ function sortThatShit(obj) {
         // the Array.prototype.sort() function may not
         // exactly be stable. #YOLODEPLOY
         return b.score - a.score;
-    })
+    });
 
     return obj;
 }
@@ -68,5 +66,5 @@ function writeToFile(obj) {
         } else {
             console.log('The file has been created!');
         }
-    })
+    });
 }
